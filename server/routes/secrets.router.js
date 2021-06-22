f22e60bd1b1aa075e63b709cd19f676262e0547f
Alex Kim <alexkim251@yahoo.com>
2021-06-22 15:30:05 -0500

    completed base and first stretch goal

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -4,15 +4,19 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   // what is the value of req.user????
+  if (req.isAuthenticated()) {
   console.log('req.user:', req.user);
 
   pool
-    .query(`SELECT * FROM "secret";`)
+    .query(`SELECT * FROM "secret" WHERE $1 >= "secret".secrecy_level  ;`, [req.user.clearance_level])
     .then((results) => res.send(results.rows))
     .catch((error) => {
       console.log('Error making SELECT for secrets:', error);
       res.sendStatus(500);
     });
+  } else {
+    res.sendStatus(403); //forbidden
+  }
 });
 
 module.exports = router;
